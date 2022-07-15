e280218de6b5ba511f656d8bd717aa4235072140
Tushar Bakshi <developer@outlook.com>
2022-07-15 21:32:58 +0530

    Fixed addition of vue

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./resources/views/**/*.blade.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
